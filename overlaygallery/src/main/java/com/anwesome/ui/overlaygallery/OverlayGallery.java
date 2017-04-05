4526f854f12a8603ee96d1ddb82b3e9238ccc1c4
Anwesh43 <anweshthecool0@gmail.com>
2017-04-05 17:51:00 +0530

    Adding OverlayGallery to add gallery in activity

---
+++ overlaygallery/src/main/java/com/anwesome/ui/overlaygallery/OverlayGallery.java
@@ -0,0 +1,36 @@
+package com.anwesome.ui.overlaygallery;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by anweshmishra on 05/04/17.
+ */
+public class OverlayGallery {
+    private List<Bitmap> bitmaps = new ArrayList<>();
+    private Activity activity;
+    private GalleryView galleryView;
+    public OverlayGallery(Activity activity, List<Bitmap> bitmaps) {
+        this.activity = activity;
+        this.bitmaps = bitmaps;
+    }
+    public void show() {
+        if(galleryView == null) {
+            galleryView = new GalleryView(activity,bitmaps);
+            activity.addContentView(galleryView,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        }
+        if(galleryView.getVisibility() == View.INVISIBLE) {
+            galleryView.setVisibility(View.VISIBLE);
+        }
+    }
+    public void hide() {
+        if(galleryView!=null) {
+            galleryView.setVisibility(View.INVISIBLE);
+        }
+    }
+}
