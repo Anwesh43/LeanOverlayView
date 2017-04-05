5be48615423858e9cb5d04b7466fc35d78bd337c
Anwesh43 <anweshthecool0@gmail.com>
2017-04-05 14:48:31 +0530

    Adding Gallery class

--- overlaygallery/src/main/java/com/anwesome/ui/overlaygallery/Gallery.java
+++ overlaygallery/src/main/java/com/anwesome/ui/overlaygallery/Gallery.java
@@ -1,8 +1,29 @@
 package com.anwesome.ui.overlaygallery;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by anweshmishra on 05/04/17.
  */
 public class Gallery {
-     
+    private List<Bitmap> bitmaps = new ArrayList<>();
+    private float x,y;
+    public Gallery(List<Bitmap> bitmaps,int w,int h) {
+        init(bitmaps,w,h);
+    }
+    private void init(List<Bitmap> bitmaps,int w,int h) {
+        for(Bitmap bitmap:bitmaps) {
+            Bitmap newBitmap = bitmap.createScaledBitmap(bitmap,w,h/2,true);
+            this.bitmaps.add(newBitmap);
+        }
+    }
+    public void draw(Canvas canvas, Paint paint) {
+        canvas.drawColor(Color.parseColor("#AAFFFFFF"));
+    }
 }
